ed5a24ba5e2b7e9f336eeed9ecb8a4a79bb0043c
gustavorafaeldev <grlrafael@outlook.com>
2022-09-22 11:27:51 -0300

    excecao personalizada para objeto nao encontrado

---
+++ src/main/java/com/rafael/bookstore/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rafael.bookstore.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String arg0) {
+        super(arg0);
+    }
+
+    public ObjectNotFoundException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+    
+}
--- src/main/java/com/rafael/bookstore/services/CategoriaService.java
+++ src/main/java/com/rafael/bookstore/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.rafael.bookstore.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,6 @@ public class CategoriaService {
     
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new com.rafael.bookstore.exceptions.ObjectNotFoundException("Objeto não encontrado! Id: "+ id+ ", Tipo: "+ Categoria.class.getName()));
     }
 }
