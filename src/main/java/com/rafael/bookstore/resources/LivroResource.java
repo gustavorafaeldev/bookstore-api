52cdee25d04c10fc58662e5039d56c65b504672b
gustavorafaeldev <grlrafael@outlook.com>
2022-09-22 17:45:51 -0300

    PUT e PATCH para Livro

--- src/main/java/com/rafael/bookstore/resources/LivroResource.java
+++ src/main/java/com/rafael/bookstore/resources/LivroResource.java
@@ -37,4 +37,15 @@ public class LivroResource {
         return ResponseEntity.ok().body(livro);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
 }
--- src/main/java/com/rafael/bookstore/services/LivroService.java
+++ src/main/java/com/rafael/bookstore/services/LivroService.java
@@ -34,4 +34,15 @@ public class LivroService {
         return repository.save(livro);
     }
 
+    public Livro update(Integer id, Livro obj) {
+        Livro newObj = findById(id);
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(Livro newObj, Livro obj) {
+        newObj.setTitulo(obj.getTitulo());
+        newObj.setNomeAutor(obj.getNomeAutor());
+        newObj.setTexto(obj.getTexto());
+    }
 }
